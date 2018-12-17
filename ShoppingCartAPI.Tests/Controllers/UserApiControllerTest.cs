73df530600cd31f5dc447bd752ec6673efa12a5c
deepanjali1094 <deepanjali.yadav@neosofttech.com>
2018-12-17 17:23:49 +0530

    Commit Test controller.

---
+++ ShoppingCartAPI.Tests/Controllers/UserApiControllerTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCartAPI.Controllers;
+using System.Net.Http;
+using System.Web.Http;
+using ShoppingCart.Entity;
+
+namespace ShoppingCartAPI.Tests.Controllers
+{
+    [TestClass]
+    public class UserApiControllerTest
+    {
+        [TestMethod]
+        public void GetUserListTest()
+        {
+            // Arrange
+            UserApiController controller = new UserApiController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // Act
+            var response = controller.GetUserList();
+
+            // Assert
+            Assert.IsNotNull(response);
+            //Assert.AreEqual(200, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetUserByIdTest()
+        {
+            // Arrange
+            UserApiController controller = new UserApiController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // Act
+            var response = controller.GetUserById(1);
+
+            // Assert
+            Assert.IsNotNull(response);
+        }
+    }
+}
--- ShoppingCartAPI/Controllers/UserApiController.cs
+++ ShoppingCartAPI/Controllers/UserApiController.cs
@@ -13,20 +13,67 @@ namespace ShoppingCartAPI.Controllers
     [RoutePrefix("api/UserAPI")]
     public class UserApiController : ApiController
     {
+        UserDAL objUserDAL = new UserDAL();
 
         /// <summary>
-        /// 2018/05/24 - Vaishnavi Soni - 
-        /// List Of Products for Home Page
+        /// 2018/12/17 - Deepanjali Yadav - 
+        /// List Of Users
         /// </summary>       
         /// <returns>HttpResponseMessage</returns>
-        /// Get: ProductsAPI/GetProducts
+        /// Get: UserAPI/GetUserList
         [HttpPost]
         [Route("GetUserList")]
         public HttpResponseMessage GetUserList()
         {
             ResponseMessage<List<STP_GetUsers_Result>> objResponseData = new ResponseMessage<List<STP_GetUsers_Result>>();
+            try
+            {
+                List<STP_GetUsers_Result> userList = objUserDAL.GetUserList().Where(com => com.IsActive == true).ToList();
 
-               
+                if (userList.Count > 0)
+                {
+                    objResponseData = ResponseHandler<STP_GetUsers_Result>.CreateResponse(objResponseData, "List Of Users", userList, HttpStatusCode.OK);
+                }
+                else
+                {
+                    objResponseData = ResponseHandler<STP_GetUsers_Result>.CreateResponse(objResponseData, "No Users Available", userList, HttpStatusCode.NoContent);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                objResponseData = ResponseHandler<STP_GetUsers_Result>.CreateErrorResponse(objResponseData);
+            }
+            return Request.CreateResponse(objResponseData.StatusCode, objResponseData);
+        }
+
+        /// <summary>
+        /// 2018/12/17 - Deepanjali Yadav - 
+        /// Get User By Id
+        /// </summary>       
+        /// <returns>HttpResponseMessage</returns>
+        /// Get: UserAPI/GetUserById
+        [HttpPost]
+        [Route("GetUserById")]
+        public HttpResponseMessage GetUserById(int Id)
+        {
+            ResponseMessage<STP_GetUserDetails_Result> objResponseData = new ResponseMessage<STP_GetUserDetails_Result>();
+            try
+            {
+                STP_GetUserDetails_Result userDetail = objUserDAL.GetUserById(Id);
+
+                if (userDetail != null)
+                {                  
+                    objResponseData = ResponseHandler<STP_GetUserDetails_Result>.CreateResponse(objResponseData, "Detail Of Users", userDetail, HttpStatusCode.OK);
+                }
+                else
+                {
+                    objResponseData = ResponseHandler<STP_GetUserDetails_Result>.CreateResponse(objResponseData, "No Users Available", userDetail, HttpStatusCode.NoContent);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                objResponseData = ResponseHandler<STP_GetUserDetails_Result>.CreateErrorResponse(objResponseData);
+            }
             return Request.CreateResponse(objResponseData.StatusCode, objResponseData);
         }
     }
