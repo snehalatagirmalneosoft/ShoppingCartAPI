42b92892c11a9a14e32396f89bfdda67702c6651
snehalatagirmal <snehalata.girmal@wwindia.com>
2018-12-19 17:42:35 +0530

    Api token base Authentication

---
+++ ShoppingCartAPI/Models/Account.cs
@@ -0,0 +1,41 @@
+﻿using ShoppingCart.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace ShoppingCartAPI.Models
+{
+    public class ApplicationDbContexts
+    {
+        // Created Shopping Cart Entity Object
+        ShoppingCartEntities db = new ShoppingCartEntities();
+        public ShoppingCartEntities ApplicationDbContext { get { return db; } }
+    }
+
+    public class Account : ApplicationDbContexts
+    {
+        public bool Login(string userName, string password)
+        {
+            try
+            {
+                var userInfo = ApplicationDbContext.UserMaster.Where(x => x.EmailId == userName).FirstOrDefault();
+                if (userInfo != null)
+                {
+                    // Encoding.ASCII.GetString(
+                    string stringPwd = userInfo.UserPassWord;
+                    return stringPwd == password;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
--- ShoppingCartAPI/Models/IdentityModels.cs
+++ ShoppingCartAPI/Models/IdentityModels.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using System.Text;
+using ShoppingCart.Entity;
 
 namespace ShoppingCartAPI.Models
 {
@@ -13,9 +15,12 @@ namespace ShoppingCartAPI.Models
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
-            // Add custom user claims here
+            // Add custom user claims here  
+
+          
             return userIdentity;
         }
+
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -24,10 +29,11 @@ namespace ShoppingCartAPI.Models
             : base("ShoppingCartEntities", throwIfV1Schema: false)
         {
         }
-        
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
         }
+    }  
+
     }
-}
